431f794b8ddaca8f7024a8e88a347034b6bfecd2
GeorgePik <george.pikramenos@hotmail.com>
2019-03-06 18:41:42 +0200

    add client-api

---
+++ src/Client-Api.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#include "App/Client.h"
+
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+
+  int id, int_msg, f;
+  if (argc > 1){
+    id = atoi(argv[1]);
+  }
+  else {
+    cout << "Enter Player Id. Exiting"<<endl;
+    exit(-1);
+  }
+
+  Client c(id, 3);
+
+  f = c.connect_to_player("127.0.0.1", 14000);
+  cout << f;
+  if (f == -1){
+    exit(-1);
+  }
+  while(1){
+  cout << "Enter message: " << endl;
+  cin >> int_msg;
+  c.send_int_to(0, int_msg);
+  cout << c.receive_int_from(0) << endl;}
+
+  return 0;
+}
